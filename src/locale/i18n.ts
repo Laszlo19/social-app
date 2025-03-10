adc87de0994ac5371afb525d0b3b9323416fe850
Laszlo19 <laszlotamas108yahoo.com@gmail.com>
2025-03-10 21:15:22 +0200

    Added more languages i18n.ts

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -50,6 +50,58 @@ import {messages as messagesVi} from '#/locale/locales/vi/messages'
 import {messages as messagesZh_CN} from '#/locale/locales/zh-CN/messages'
 import {messages as messagesZh_HK} from '#/locale/locales/zh-HK/messages'
 import {messages as messagesZh_TW} from '#/locale/locales/zh-TW/messages'
+
+// New language imports
+import {messages as messagesAb} from '#/locale/locales/ab/messages'
+import {messages as messagesAf} from '#/locale/locales/af/messages'
+import {messages as messagesAm} from '#/locale/locales/am/messages'
+import {messages as messagesAr} from '#/locale/locales/ar/messages'
+import {messages as messagesAz} from '#/locale/locales/az/messages'
+import {messages as messagesBg} from '#/locale/locales/bg/messages'
+import {messages as messagesBn} from '#/locale/locales/bn/messages'
+import {messages as messagesBs} from '#/locale/locales/bs/messages'
+import {messages as messagesCo} from '#/locale/locales/co/messages'
+import {messages as messagesCs} from '#/locale/locales/cs/messages'
+import {messages as messagesEt} from '#/locale/locales/et/messages'
+import {messages as messagesFa} from '#/locale/locales/fa/messages'
+import {messages as messagesFy} from '#/locale/locales/fy/messages'
+import {messages as messagesHe} from '#/locale/locales/he/messages'
+import {messages as messagesHr} from '#/locale/locales/hr/messages'
+import {messages as messagesHy} from '#/locale/locales/hy/messages'
+import {messages as messagesIs} from '#/locale/locales/is/messages'
+import {messages as messagesKa} from '#/locale/locales/ka/messages'
+import {messages as messagesKk} from '#/locale/locales/kk/messages'
+import {messages as messagesKn} from '#/locale/locales/kn/messages'
+import {messages as messagesKrc} from '#/locale/locales/krc/messages'
+import {messages as messagesKy} from '#/locale/locales/ky/messages'
+import {messages as messagesLb} from '#/locale/locales/lb/messages'
+import {messages as messagesLo} from '#/locale/locales/lo/messages'
+import {messages as messagesLt} from '#/locale/locales/lt/messages'
+import {messages as messagesLv} from '#/locale/locales/lv/messages'
+import {messages as messagesMi} from '#/locale/locales/mi/messages'
+import {messages as messagesMk} from '#/locale/locales/mk/messages'
+import {messages as messagesMn} from '#/locale/locales/mn/messages'
+import {messages as messagesMs} from '#/locale/locales/ms/messages'
+import {messages as messagesNo} from '#/locale/locales/no/messages'
+import {messages as messagesPt_PT} from '#/locale/locales/pt-PT/messages'
+import {messages as messagesSe} from '#/locale/locales/se/messages'
+import {messages as messagesSk} from '#/locale/locales/sk/messages'
+import {messages as messagesSl} from '#/locale/locales/sl/messages'
+import {messages as messagesSq} from '#/locale/locales/sq/messages'
+import {messages as messagesSr} from '#/locale/locales/sr/messages'
+import {messages as messagesSw} from '#/locale/locales/sw/messages'
+import {messages as messagesTa} from '#/locale/locales/ta/messages'
+import {messages as messagesTe} from '#/locale/locales/te/messages'
+import {messages as messagesTg} from '#/locale/locales/tg/messages'
+import {messages as messagesTl} from '#/locale/locales/tl/messages'
+import {messages as messagesTt} from '#/locale/locales/tt/messages'
+import {messages as messagesUr} from '#/locale/locales/ur/messages'
+import {messages as messagesUz} from '#/locale/locales/uz/messages'
+import {messages as messagesXh} from '#/locale/locales/xh/messages'
+import {messages as messagesYo} from '#/locale/locales/yo/messages'
+import {messages as messagesYue} from '#/locale/locales/yue/messages'
+import {messages as messagesZu} from '#/locale/locales/zu/messages'
+
 import {useLanguagePrefs} from '#/state/preferences'
 
 /**
@@ -355,6 +407,203 @@ export async function dynamicActivate(locale: AppLanguage) {
         import('@formatjs/intl-pluralrules/locale-data/zh'),
         import('@formatjs/intl-numberformat/locale-data/zh'),
       ])
+      break
+    }
+    // New languages
+    case AppLanguage.ab: {
+      i18n.loadAndActivate({locale, messages: messagesAb})
+      break
+    }
+    case AppLanguage.af: {
+      i18n.loadAndActivate({locale, messages: messagesAf})
+      break
+    }
+    case AppLanguage.am: {
+      i18n.loadAndActivate({locale, messages: messagesAm})
+      break
+    }
+    case AppLanguage.ar: {
+      i18n.loadAndActivate({locale, messages: messagesAr})
+      break
+    }
+    case AppLanguage.az: {
+      i18n.loadAndActivate({locale, messages: messagesAz})
+      break
+    }
+    case AppLanguage.bg: {
+      i18n.loadAndActivate({locale, messages: messagesBg})
+      break
+    }
+    case AppLanguage.bn: {
+      i18n.loadAndActivate({locale, messages: messagesBn})
+      break
+    }
+    case AppLanguage.bs: {
+      i18n.loadAndActivate({locale, messages: messagesBs})
+      break
+    }
+    case AppLanguage.co: {
+      i18n.loadAndActivate({locale, messages: messagesCo})
+      break
+    }
+    case AppLanguage.cs: {
+      i18n.loadAndActivate({locale, messages: messagesCs})
+      break
+    }
+    case AppLanguage.et: {
+      i18n.loadAndActivate({locale, messages: messagesEt})
+      break
+    }
+    case AppLanguage.fa: {
+      i18n.loadAndActivate({locale, messages: messagesFa})
+      break
+    }
+    case AppLanguage.fy: {
+      i18n.loadAndActivate({locale, messages: messagesFy})
+      break
+    }
+    case AppLanguage.he: {
+      i18n.loadAndActivate({locale, messages: messagesHe})
+      break
+    }
+    case AppLanguage.hr: {
+      i18n.loadAndActivate({locale, messages: messagesHr})
+      break
+    }
+    case AppLanguage.hy: {
+      i18n.loadAndActivate({locale, messages: messagesHy})
+      break
+    }
+    case AppLanguage.is: {
+      i18n.loadAndActivate({locale, messages: messagesIs})
+      break
+    }
+    case AppLanguage.ka: {
+      i18n.loadAndActivate({locale, messages: messagesKa})
+      break
+    }
+    case AppLanguage.kk: {
+      i18n.loadAndActivate({locale, messages: messagesKk})
+      break
+    }
+    case AppLanguage.kn: {
+      i18n.loadAndActivate({locale, messages: messagesKn})
+      break
+    }
+    case AppLanguage.krc: {
+      i18n.loadAndActivate({locale, messages: messagesKrc})
+      break
+    }
+    case AppLanguage.ky: {
+      i18n.loadAndActivate({locale, messages: messagesKy})
+      break
+    }
+    case AppLanguage.lb: {
+      i18n.loadAndActivate({locale, messages: messagesLb})
+      break
+    }
+    case AppLanguage.lo: {
+      i18n.loadAndActivate({locale, messages: messagesLo})
+      break
+    }
+    case AppLanguage.lt: {
+      i18n.loadAndActivate({locale, messages: messagesLt})
+      break
+    }
+    case AppLanguage.lv: {
+      i18n.loadAndActivate({locale, messages: messagesLv})
+      break
+    }
+    case AppLanguage.mi: {
+      i18n.loadAndActivate({locale, messages: messagesMi})
+      break
+    }
+    case AppLanguage.mk: {
+      i18n.loadAndActivate({locale, messages: messagesMk})
+      break
+    }
+    case AppLanguage.mn: {
+      i18n.loadAndActivate({locale, messages: messagesMn})
+      break
+    }
+    case AppLanguage.ms: {
+      i18n.loadAndActivate({locale, messages: messagesMs})
+      break
+    }
+    case AppLanguage.no: {
+      i18n.loadAndActivate({locale, messages: messagesNo})
+      break
+    }
+    case AppLanguage.pt_PT: {
+      i18n.loadAndActivate({locale, messages: messagesPt_PT})
+      break
+    }
+    case AppLanguage.se: {
+      i18n.loadAndActivate({locale, messages: messagesSe})
+      break
+    }
+    case AppLanguage.sk: {
+      i18n.loadAndActivate({locale, messages: messagesSk})
+      break
+    }
+    case AppLanguage.sl: {
+      i18n.loadAndActivate({locale, messages: messagesSl})
+      break
+    }
+    case AppLanguage.sq: {
+      i18n.loadAndActivate({locale, messages: messagesSq})
+      break
+    }
+    case AppLanguage.sr: {
+      i18n.loadAndActivate({locale, messages: messagesSr})
+      break
+    }
+    case AppLanguage.sw: {
+      i18n.loadAndActivate({locale, messages: messagesSw})
+      break
+    }
+    case AppLanguage.ta: {
+      i18n.loadAndActivate({locale, messages: messagesTa})
+      break
+    }
+    case AppLanguage.te: {
+      i18n.loadAndActivate({locale, messages: messagesTe})
+      break
+    }
+    case AppLanguage.tg: {
+      i18n.loadAndActivate({locale, messages: messagesTg})
+      break
+    }
+    case AppLanguage.tl: {
+      i18n.loadAndActivate({locale, messages: messagesTl})
+      break
+    }
+    case AppLanguage.tt: {
+      i18n.loadAndActivate({locale, messages: messagesTt})
+      break
+    }
+    case AppLanguage.ur: {
+      i18n.loadAndActivate({locale, messages: messagesUr})
+      break
+    }
+    case AppLanguage.uz: {
+      i18n.loadAndActivate({locale, messages: messagesUz})
+      break
+    }
+    case AppLanguage.xh: {
+      i18n.loadAndActivate({locale, messages: messagesXh})
+      break
+    }
+    case AppLanguage.yo: {
+      i18n.loadAndActivate({locale, messages: messagesYo})
+      break
+    }
+    case AppLanguage.yue: {
+      i18n.loadAndActivate({locale, messages: messagesYue})
+      break
+    }
+    case AppLanguage.zu: {
+      i18n.loadAndActivate({locale, messages: messagesZu})
       break
     }
     default: {
