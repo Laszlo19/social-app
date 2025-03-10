cad69da8bb55bee90bef2324e69f7220dfded75b
Laszlo19 <laszlotamas108yahoo.com@gmail.com>
2025-03-10 21:19:24 +0200

    Added more langauges i18n.web.ts

--- src/locale/i18n.web.ts
+++ src/locale/i18n.web.ts
@@ -164,6 +164,203 @@ export async function dynamicActivate(locale: AppLanguage) {
       mod = await import(`./locales/zh-TW/messages`)
       break
     }
+    // New languages
+    case AppLanguage.ab: {
+      mod = await import(`./locales/ab/messages`)
+      break
+    }
+    case AppLanguage.af: {
+      mod = await import(`./locales/af/messages`)
+      break
+    }
+    case AppLanguage.am: {
+      mod = await import(`./locales/am/messages`)
+      break
+    }
+    case AppLanguage.ar: {
+      mod = await import(`./locales/ar/messages`)
+      break
+    }
+    case AppLanguage.az: {
+      mod = await import(`./locales/az/messages`)
+      break
+    }
+    case AppLanguage.bg: {
+      mod = await import(`./locales/bg/messages`)
+      break
+    }
+    case AppLanguage.bn: {
+      mod = await import(`./locales/bn/messages`)
+      break
+    }
+    case AppLanguage.bs: {
+      mod = await import(`./locales/bs/messages`)
+      break
+    }
+    case AppLanguage.co: {
+      mod = await import(`./locales/co/messages`)
+      break
+    }
+    case AppLanguage.cs: {
+      mod = await import(`./locales/cs/messages`)
+      break
+    }
+    case AppLanguage.et: {
+      mod = await import(`./locales/et/messages`)
+      break
+    }
+    case AppLanguage.fa: {
+      mod = await import(`./locales/fa/messages`)
+      break
+    }
+    case AppLanguage.fy: {
+      mod = await import(`./locales/fy/messages`)
+      break
+    }
+    case AppLanguage.he: {
+      mod = await import(`./locales/he/messages`)
+      break
+    }
+    case AppLanguage.hr: {
+      mod = await import(`./locales/hr/messages`)
+      break
+    }
+    case AppLanguage.hy: {
+      mod = await import(`./locales/hy/messages`)
+      break
+    }
+    case AppLanguage.is: {
+      mod = await import(`./locales/is/messages`)
+      break
+    }
+    case AppLanguage.ka: {
+      mod = await import(`./locales/ka/messages`)
+      break
+    }
+    case AppLanguage.kk: {
+      mod = await import(`./locales/kk/messages`)
+      break
+    }
+    case AppLanguage.kn: {
+      mod = await import(`./locales/kn/messages`)
+      break
+    }
+    case AppLanguage.krc: {
+      mod = await import(`./locales/krc/messages`)
+      break
+    }
+    case AppLanguage.ky: {
+      mod = await import(`./locales/ky/messages`)
+      break
+    }
+    case AppLanguage.lb: {
+      mod = await import(`./locales/lb/messages`)
+      break
+    }
+    case AppLanguage.lo: {
+      mod = await import(`./locales/lo/messages`)
+      break
+    }
+    case AppLanguage.lt: {
+      mod = await import(`./locales/lt/messages`)
+      break
+    }
+    case AppLanguage.lv: {
+      mod = await import(`./locales/lv/messages`)
+      break
+    }
+    case AppLanguage.mi: {
+      mod = await import(`./locales/mi/messages`)
+      break
+    }
+    case AppLanguage.mk: {
+      mod = await import(`./locales/mk/messages`)
+      break
+    }
+    case AppLanguage.mn: {
+      mod = await import(`./locales/mn/messages`)
+      break
+    }
+    case AppLanguage.ms: {
+      mod = await import(`./locales/ms/messages`)
+      break
+    }
+    case AppLanguage.no: {
+      mod = await import(`./locales/no/messages`)
+      break
+    }
+    case AppLanguage.pt_PT: {
+      mod = await import(`./locales/pt-PT/messages`)
+      break
+    }
+    case AppLanguage.se: {
+      mod = await import(`./locales/se/messages`)
+      break
+    }
+    case AppLanguage.sk: {
+      mod = await import(`./locales/sk/messages`)
+      break
+    }
+    case AppLanguage.sl: {
+      mod = await import(`./locales/sl/messages`)
+      break
+    }
+    case AppLanguage.sq: {
+      mod = await import(`./locales/sq/messages`)
+      break
+    }
+    case AppLanguage.sr: {
+      mod = await import(`./locales/sr/messages`)
+      break
+    }
+    case AppLanguage.sw: {
+      mod = await import(`./locales/sw/messages`)
+      break
+    }
+    case AppLanguage.ta: {
+      mod = await import(`./locales/ta/messages`)
+      break
+    }
+    case AppLanguage.te: {
+      mod = await import(`./locales/te/messages`)
+      break
+    }
+    case AppLanguage.tg: {
+      mod = await import(`./locales/tg/messages`)
+      break
+    }
+    case AppLanguage.tl: {
+      mod = await import(`./locales/tl/messages`)
+      break
+    }
+    case AppLanguage.tt: {
+      mod = await import(`./locales/tt/messages`)
+      break
+    }
+    case AppLanguage.ur: {
+      mod = await import(`./locales/ur/messages`)
+      break
+    }
+    case AppLanguage.uz: {
+      mod = await import(`./locales/uz/messages`)
+      break
+    }
+    case AppLanguage.xh: {
+      mod = await import(`./locales/xh/messages`)
+      break
+    }
+    case AppLanguage.yo: {
+      mod = await import(`./locales/yo/messages`)
+      break
+    }
+    case AppLanguage.yue: {
+      mod = await import(`./locales/yue/messages`)
+      break
+    }
+    case AppLanguage.zu: {
+      mod = await import(`./locales/zu/messages`)
+      break
+    }
     default: {
       mod = await import(`./locales/en/messages`)
       break
